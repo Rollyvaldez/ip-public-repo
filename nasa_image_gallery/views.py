0f97e5be929db7171e739428be644cb3413ec1df
SantiagoAbalde <santiago.abalde@outlook.es>
2024-06-24 01:15:14 -0300

    Update views.py

--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -25,7 +25,7 @@ def home(request):
     # (*) este último, solo si se desarrolló el opcional de favoritos; caso contrario, será un listado vacío [].
     images = []
     favourite_list = []
-    images,favourite_list = GetAllImagesAndFavouriteList(request) # 2 variables separadas por una coma a las que se le asigna la funcion anterior,
+    images,favourite_list = getAllImagesAndFavouriteList(request) # 2 variables separadas por una coma a las que se le asigna la funcion anterior,
                                                                   # ya que esta retorna ambas listas en una misma linea de codigo
     return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list} ) # La funcion render toma 3 parametros y retorna una respuesta http
     # primero el objeto(request) a peticion del usuario, segundo la plantilla que se va a renderizar 'home.html' y
